51d6a0abbd3cdb76d80f2d5e8f1b83cfe4055172
Live session user <ubuntu@ubuntu.(none)>
2012-10-09 19:18:53 -0500

    Initial commit

---
+++ tickets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '1640f6b1ffa4d1d91dbf6ee1649720ad6289c692f273af2eeb5eeb7d8f3c84272b4aa10198b2cdc443ee30668d4d234430a3a1c55b5557d95ce0783ca1e565af'
